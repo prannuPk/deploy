a86e11956043689ffc0bb7045978fd3b8402a691
prannu1218 <124121180+prannu1218@users.noreply.github.com>
2024-10-28 17:08:47 +0530

    Update landing.jsx

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -7,47 +7,53 @@ export default function LandingPage() {
     const router = useNavigate();
 
     return (
-        <div className='landingPageContainer'>
-            <nav>
-                <div className='navHeader'>
-                    <h2>Apna Video Call</h2>
-                </div>
-                <div className='navlist'>
-                    <p onClick={() => {
-                        router("/aljk23")
-                    }}>Join as Guest</p>
-                    <p onClick={() => {
-                        router("/auth")
-
-                    }}>Register</p>
-                    <div onClick={() => {
-                        router("/auth")
-
-                    }} role='button'>
-                        <p>Login</p>
-                    </div>
-                </div>
-            </nav>
-
-
-            <div className="landingMainContainer">
-                <div>
-                    <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
-
-                    <p>Cover a distance by Apna Video Call</p>
-                    <div role='button'>
-                        <Link to={"/auth"}>Get Started</Link>
-                    </div>
-                </div>
-                <div>
-
-                    <img src="/mobile.png" alt="" />
-
-                </div>
+      <div className="landingPageContainer">
+        <nav>
+          <div className="navHeader">
+            <h2>Link Up</h2>
+          </div>
+          <div className="navlist">
+            <p
+              onClick={() => {
+                router("/aljk23");
+              }}
+            >
+              Join as Guest
+            </p>
+            <p
+              onClick={() => {
+                router("/auth");
+              }}
+            >
+              Register
+            </p>
+            <div
+              onClick={() => {
+                router("/auth");
+              }}
+              role="button"
+            >
+              <p>Login</p>
             </div>
+          </div>
+        </nav>
 
+        <div className="landingMainContainer">
+          <div>
+            <h1>
+              <span style={{ color: "#FFA500" }}>Connect</span> with your loved
+              Ones
+            </h1>
 
-
+            <p>Cover a distance by LinkUp</p>
+            <div role="button">
+              <Link to={"/auth"}>Get Started</Link>
+            </div>
+          </div>
+          <div>
+            <img src="/videoCall.jpg" alt="" />
+          </div>
         </div>
-    )
+      </div>
+    );
 }
