7002b223e113d356b774f059ec8d02b5f2d5e9ae
prannu1218 <124121180+prannu1218@users.noreply.github.com>
2024-10-28 13:22:02 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,8 @@
 import express from "express";
 import { createServer } from "node:http";
-
 import { Server } from "socket.io";
-
 import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
-
 import cors from "cors";
 import userRoutes from "./routes/users.routes.js";
 
@@ -13,29 +10,35 @@ const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
+app.set("port", process.env.PORT || 8000);
 
-app.set("port", (process.env.PORT || 8000))
-app.use(cors());
+const corsOptions = {
+   origin: 'https://deploy1-1-al14.onrender.com', 
+   methods: 'GET, POST, PUT, DELETE',
+   allowedHeaders: 'Content-Type, Authorization',
+   credentials: true
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); 
+
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
-
-app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/users", userRoutes); 
 
 const start = async () => {
-    app.set("mongo_user")
-    const connectionDb = await mongoose.connect(
-      "mongodb+srv://praneethapkr1218:iompProject@cluster0.dakiq.mongodb.net/"
-    );
-
-    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
-    server.listen(app.get("port"), () => {
-        console.log("LISTENIN ON PORT 8000")
-    });
-
-
-
-}
-
-
+   try {
+      const connectionDb = await mongoose.connect(
+         "mongodb+srv://praneethapkr1218:iompProject@cluster0.dakiq.mongodb.net/"
+      );
+      console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`);
+      
+      server.listen(app.get("port"), () => {
+         console.log(`Listening on port ${app.get("port")}`);
+      });
+   } catch (error) {
+      console.error("Error connecting to the database:", error);
+   }
+};
 
 start();
