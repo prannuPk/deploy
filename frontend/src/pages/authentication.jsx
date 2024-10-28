63dac33a4559ce59b68379dd7221ae0ab5a1d6ed
prannu1218 <124121180+prannu1218@users.noreply.github.com>
2024-10-28 17:22:48 +0530

    Update authentication.jsx

--- frontend/src/pages/authentication.jsx
+++ frontend/src/pages/authentication.jsx
@@ -76,7 +76,7 @@ export default function Authentication() {
                     sm={4}
                     md={7}
                     sx={{
-                        backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+                        backgroundImage: `url(${process.env.PUBLIC_URL}/authPic.jpeg)`,
                         backgroundRepeat: 'no-repeat',
                         backgroundColor: (t) =>
                             t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
