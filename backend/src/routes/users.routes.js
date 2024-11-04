9307f7a684a7a50606db31cc7f942776de6bfcce
praneeth1218 <praneetharceus@gmail.com>
2024-11-04 15:35:47 +0530

    Update users.routes.js

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -13,6 +13,7 @@ router.route("/login").post(login);
 router.route("/register").post(register);
 router.route("/add_to_activity").post(addToHistory);
 router.route("/get_all_activity").get(getUserHistory);
-router.route("/join_meeting").post(joinMeeting); // New route for joining a meeting
+router.route("/join_meeting").post(joinMeeting); // Route for joining a meeting
+router.route("/meetings/validate").post(joinMeeting); // New route for validation
 
 export default router;
