fe94b71c9f33d9ee6305d064e781ec81fe8327a3
praneeth1218 <praneetharceus@gmail.com>
2024-11-04 11:04:34 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,32 @@
-import './App.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import LandingPage from './pages/landing';
-import Authentication from './pages/authentication';
-import { AuthProvider } from './contexts/AuthContext';
-import VideoMeetComponent from './pages/VideoMeet';
-import HomeComponent from './pages/home';
-import History from './pages/history';
+import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import LandingPage from "./pages/landing";
+import Authentication from "./pages/authentication";
+import { AuthProvider } from "./contexts/AuthContext";
+import VideoMeetComponent from "./pages/VideoMeet";
+import HomeComponent from "./pages/home";
+import History from "./pages/history";
+import withAuth from "./utils/withAuth"; // Import the withAuth HOC
+
+// Wrap your VideoMeetComponent and History with the withAuth HOC
+const AuthVideoMeetComponent = withAuth(VideoMeetComponent);
+const AuthHistory = withAuth(History);
 
 function App() {
   return (
     <div className="App">
-
       <Router>
-
         <AuthProvider>
-
-
           <Routes>
-
-            <Route path='/' element={<LandingPage />} />
-
-            <Route path='/auth' element={<Authentication />} />
-
-            <Route path='/home's element={<HomeComponent />} />
-            <Route path='/history' element={<History />} />
-            <Route path='/:url' element={<VideoMeetComponent />} />
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/auth" element={<Authentication />} />
+            <Route path="/home" element={<HomeComponent />} />
+            <Route path="/history" element={<AuthHistory />} />{" "}
+            {/* Protect the History route */}
+            <Route path="/:url" element={<AuthVideoMeetComponent />} />{" "}
+            {/* Protect the VideoMeet route */}
           </Routes>
         </AuthProvider>
-
       </Router>
     </div>
   );
