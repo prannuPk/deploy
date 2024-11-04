f170b58b66d7aa3ffc2c9bf18f424f309f06a9ba
praneeth1218 <praneetharceus@gmail.com>
2024-11-04 11:01:26 +0530

    Update users.routes.js

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,13 +1,18 @@
 import { Router } from "express";
-import { addToHistory, getUserHistory, login, register } from "../controllers/user.controller.js";
-
-
+import {
+  addToHistory,
+  getUserHistory,
+  login,
+  register,
+  joinMeeting,
+} from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.route("/login").post(login)
-router.route("/register").post(register)
-router.route("/add_to_activity").post(addToHistory)
-router.route("/get_all_activity").get(getUserHistory)
+router.route("/login").post(login);
+router.route("/register").post(register);
+router.route("/add_to_activity").post(addToHistory);
+router.route("/get_all_activity").get(getUserHistory);
+router.route("/join_meeting").post(joinMeeting); // New route for joining a meeting
 
 export default router;
