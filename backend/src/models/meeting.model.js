038da18435862cd3345b5d7d5c3c545c703cde3b
praneeth1218 <praneetharceus@gmail.com>
2024-11-04 20:23:29 +0530

    Update meeting.model.js

--- backend/src/models/meeting.model.js
+++ backend/src/models/meeting.model.js
@@ -1,9 +1,9 @@
 import mongoose, { Schema } from "mongoose";
 
 const meetingSchema = new Schema({
-  user_id: { type: String },
-  meetingCode: { type: String, required: true },
-  password: { type: String, required: true }, // Added password field
+  user_id: { type: String, required: true }, // Ensure user_id is required
+  meetingCode: { type: String, required: true, unique: true }, // Unique constraint to prevent duplicates
+  password: { type: String, required: true }, // Password field to store hashed password
   date: { type: Date, default: Date.now, required: true },
 });
 
