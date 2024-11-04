7d86573937dc84b4d5734cd7e26e526b21a6d6b4
praneeth1218 <praneetharceus@gmail.com>
2024-11-04 11:01:02 +0530

    Update meeting.model.js

--- backend/src/models/meeting.model.js
+++ backend/src/models/meeting.model.js
@@ -1,13 +1,11 @@
 import mongoose, { Schema } from "mongoose";
 
-
-const meetingSchema = new Schema(
-    {
-        user_id: { type: String },
-        meetingCode: { type: String, required: true },
-        date: { type: Date, default: Date.now, required: true }
-    }
-)
+const meetingSchema = new Schema({
+  user_id: { type: String },
+  meetingCode: { type: String, required: true },
+  password: { type: String, required: true }, // Added password field
+  date: { type: Date, default: Date.now, required: true },
+});
 
 const Meeting = mongoose.model("Meeting", meetingSchema);
 
