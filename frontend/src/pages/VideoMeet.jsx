0e629827522992fc593fc6636060bfe03a853a41
praneeth1218 <praneetharceus@gmail.com>
2024-11-04 12:04:48 +0530

    Update VideoMeet.jsx

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import io from "socket.io-client";
-import { useSocket } from "../context/SocketContext";
+import { useSocket } from "../contexts/SocketContext";
 import { Badge, IconButton, TextField } from '@mui/material';
 import { Button } from '@mui/material';
 import VideocamIcon from '@mui/icons-material/Videocam';
