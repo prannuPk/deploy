49e15ffa196bbbf1d425303c76dd12d54ccee7ae
praneeth1218 <praneetharceus@gmail.com>
2024-11-04 16:42:15 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,10 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import LandingPage from "./pages/landing";
 import Authentication from "./pages/authentication";
 import { AuthProvider } from "./contexts/AuthContext";
-import { SocketProvider } from "./contexts/SocketContext"; // Import the SocketProvider
+import { SocketProvider } from "./contexts/SocketContext"; 
 import VideoMeetComponent from "./pages/VideoMeet";
 import HomeComponent from "./pages/home";
 import History from "./pages/history";
-import withAuth from "./utils/withAuth"; // Import the withAuth HOC
-
-// Wrap your VideoMeetComponent and History with the withAuth HOC
-const AuthVideoMeetComponent = withAuth(VideoMeetComponent);
-const AuthHistory = withAuth(History);
 
 function App() {
   return (
@@ -19,16 +14,12 @@ function App() {
       <Router>
         <AuthProvider>
           <SocketProvider>
-            {" "}
-            {/* Wrap with SocketProvider */}
             <Routes>
               <Route path="/" element={<LandingPage />} />
               <Route path="/auth" element={<Authentication />} />
               <Route path="/home" element={<HomeComponent />} />
-              <Route path="/history" element={<AuthHistory />} />{" "}
-              {/* Protect the History route */}
-              <Route path="/:url" element={<AuthVideoMeetComponent />} />{" "}
-              {/* Protect the VideoMeet route */}
+              <Route path="/history" element={<History />} /> 
+              <Route path="/:url" element={<VideoMeetComponent />} /> 
             </Routes>
           </SocketProvider>
         </AuthProvider>
