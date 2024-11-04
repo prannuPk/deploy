b0e266e2d774719f99c7e7389dd3cd3318a65468
praneeth1218 <praneetharceus@gmail.com>
2024-11-04 11:59:47 +0530

    Create SocketContext.jsx

---
+++ frontend/src/contexts/SocketContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import io from "socket.io-client";
+import server from "../environment";
+
+export const SocketContext = createContext();
+
+const socket = io(`${server}api/v1/socket`); // Adjust the endpoint as needed
+
+export const SocketProvider = ({ children }) => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    return () => {
+      socket.off("message");
+    };
+  }, []);
+
+  const sendMessage = (message) => {
+    socket.emit("message", message);
+  };
+
+  const data = {
+    messages,
+    sendMessage,
+  };
+
+  return (
+    <SocketContext.Provider value={data}>{children}</SocketContext.Provider>
+  );
+};
+
+// Hook to use the socket context
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
